09694980ace3066226da5439c1bdf7067e3f8285
Frederico Schnekenberg <fred.schnek@gmail.com>
2015-12-24 09:33:37 -0200

    Initial Commit

---
+++ Recipez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipez
+//
+//  Created by Frederico Schnekenberg on 24/12/15.
+//  Copyright © 2015 Frederico Schnekenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
